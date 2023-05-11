4560c4daeb6efe170c2f407c4c68c1acdd65cd77
Mario <101608787+marioCoding@users.noreply.github.com>
2023-05-11 11:50:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", init)
 
 async function init() {
-    let disneyChar = await fetch("https://api.disneyapi.dev/character/308/")
+    let disneyChar = await fetch("https://api.disneyapi.dev/character?name=Mickey%20Mouse")
     let character = await disneyChar.json();
     try {
         const nameElement = document.createElement("h3");
         // access the name using character.DATA.name
         // data is important here because it takes you throught the data object
         // and into the "name" object property
-        nameElement.textContent = `${await character.data._id}`;
+        nameElement.textContent = `${await character.data[1].videoGames}`;
         // make reference to the html container where the info
         // will be displayed
         const container = document.querySelector("#container")
